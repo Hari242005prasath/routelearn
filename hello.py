ad35d49de6ec049f58e3f839a4cad8b5ef2ba31a
Hari242005prasath <160257595+Hari242005prasath@users.noreply.github.com>
2024-04-22 09:53:04 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,14 @@
 import streamlit as st
 
 def greet_user(name):
-    return f"Hello, {name}! Welcome to our app."
+    return f"Hi {name} welcome to rmk"
 
-def main():
-    st.title("Routelearn")
 
-    # Get user input for their name
-    user_name = st.text_input("Enter your name:")
+st.title("Routelearn")
 
-    if user_name:
+user_name = st.text_input("Enter your name:")
+if user_name:
         # Display a greeting message
-        greeting_message = greet_user(user_name)
-        st.write(greeting_message)
+    greeting_message = greet_user(user_name)
+    st.write(greeting_message)
 
-if __name__ == "__main__":
-    main()
